b5e54b9bdab1b62c8981edf374387ce27e8a1e96
장진우 <arzz@arzz.com>
2017-01-31 19:19:50 +0900

    캐시 적용

--- widgets/list/index.php
+++ widgets/list/index.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * 이 파일은 순천향대 연동모듈의 일부입니다. (https://www.imodule.kr)
+ * 이 파일은 iModule 배너모듈 일부입니다. (https://www.imodule.kr)
  *
  * 배너 템플릿에 따라 배너컨텐츠를 보여준다.
  * 
- * @file /modules/schu/widgets/banner/index.php
+ * @file /modules/banner/widgets/banner/index.php
  * @author Arzz (arzz@arzz.com)
  * @license MIT License
  * @version 3.0.0.160910
@@ -17,9 +17,9 @@ $cache = $Widget->getValue('cache');
 
 if ($Widget->checkCache() < time() - $cache) {
 	$banners = $me->getBanners($group);
-//	$Widget->storeCache(json_encode($banners,JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK));
+	$Widget->storeCache(json_encode($banners,JSON_UNESCAPED_UNICODE | JSON_NUMERIC_CHECK));
 } else {
-	$lists = json_decode($Widget->getCache());
+	$banners = json_decode($Widget->getCache());
 }
 
 return $Widget->getTemplet()->getContext('index',get_defined_vars());
