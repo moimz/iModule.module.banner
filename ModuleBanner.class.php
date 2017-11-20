8695f7cee830e5750fcd99c69ad0a382b090a35a
arzzcom <arzz@arzz.com>
2017-11-20 23:04:35 +0900

    이벤트 처리 변경

--- ModuleBanner.class.php
+++ ModuleBanner.class.php
@@ -102,7 +102,7 @@ class ModuleBanner {
 		/**
 		 * 이벤트를 호출한다.
 		 */
-		$this->IM->fireEvent('beforeGetApi','board',$api,$values,null);
+		$this->IM->fireEvent('beforeGetApi','board',$api,$values);
 		
 		/**
 		 * 모듈의 api 폴더에 $api 에 해당하는 파일이 있을 경우 불러온다.
